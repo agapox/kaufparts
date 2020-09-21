52fdd85203c1097a61b466b9e7f7f76dabb0f30d
jramirez <jose.ramirez@siigroup.cl>
2020-09-21 00:49:56 -0300

    header & mobile header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,9 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isMobileMenuOpen: boolean = false;
+
+  menuItems: Array<{ title: string, items: Array<string> }> = [
+    {
+      title: 'inicio',
+      items: []
+    },
+    {
+      title: "repuestos",
+      items: [
+        "dirección",
+        "bomba de dirección",
+        "caja de dirección",
+        "columna de dirección y volante",
+        "ejes",
+        "exterior",
+        "interior",
+        "motor",
+        "refrigeración",
+        "aire acondicionado y calefacción",
+        "frenos",
+        "sistema eléctrico",
+        "suspensión",
+        "transmisión",
+      ]
+    },
+    {
+      title: "genérico",
+      items: []
+    },
+    {
+      title: "boutique",
+      items: []
+    },
+    {
+      title: "ofertas",
+      items: []
+    }
+    ,
+    {
+      title: "generadores",
+      items: []
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onHamClicked() {
+    console.log('onHamClicked()')
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
   }
 
 }
